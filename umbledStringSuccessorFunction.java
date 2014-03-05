18733dcb37e201c21d5c21c9d6fa0310d6eda1a2
Joe <Jwise0312@gmail.com>
2014-03-05 03:57:26 +0000

    Create umbledStringSuccessorFunction.java

---
+++ umbledStringSuccessorFunction.java
@@ -0,0 +1,21 @@
+package rp13.search.problem.string;
+
+import java.util.List;
+
+import rp13.search.interfaces.SuccessorFunction;
+import rp13.search.util.ActionStatePair;
+
+public class JumbledStringSuccessorFunction implements SuccessorFunction<Move, JumbledString> 
+{
+	public void getSuccessors(JumbledString _state,List<ActionStatePair<Move, JumbledString>> _successors) 
+	{
+		
+		
+	}
+	
+	public static void main(String[] args) 
+	{
+		
+
+	}
+}
